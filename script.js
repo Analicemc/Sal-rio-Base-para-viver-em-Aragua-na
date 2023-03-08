b460e94ab1202b86789e16693c3c673d5b9c8403
Analice M. Costa <87390603+Analicemc@users.noreply.github.com>
2023-03-07 22:08:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var aluguel = document.getElementById('aluguel');
+var agua = document.getElementById('agua');
+var energia = document.getElementById('energia');
+var educacao = document.getElementById('educacao');
+var internet = document.getElementById('internet');
+var lixo = document.getElementById('lixo');
+var iptu = document.getElementById('iptu');
+
+var alimentacao = document.getElementById('alimentacao');
+var transporte = document.getElementById('transporte');
+var lazer = document.getElementById('lazer');
+var saude = document.getElementById('saude');
+var impostos = document.getElementById('impostos');
+var cuidados = document.getElementById('cuidados');
+var outros = document.getElementById('outros');
+
+const btn = document.getElementById('calcula');
+btn.addEventListener('click', calculaSalarioIdeal);
+
+const res = document.getElementById('res');
+
+function calculaSalarioIdeal(){
+    var sal = aluguel.valueAsNumber + agua.valueAsNumber + energia.valueAsNumber + educacao.valueAsNumber + internet.valueAsNumber + alimentacao.valueAsNumber + lixo.valueAsNumber + iptu.valueAsNumber + transporte.valueAsNumber + lazer.valueAsNumber + saude.valueAsNumber + impostos.valueAsNumber + cuidados.valueAsNumber + outros.valueAsNumber;
+    res.textContent += ` R$ ${sal}`;
+}
